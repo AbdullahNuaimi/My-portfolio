2bc5ebab24ce067fbd25cbf30563c2d16f323fac
AbdullahNuaimi <181004@ppu.edu.ps>
2023-07-12 15:47:36 +0300

    tailwind playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-6xl'>TailwindTest</h1>
-      <h1 className='text-9xl'>TailwindTest</h1>
+      <div className="p-6 max-w-sm mx-auto bg-gradient-to-r justify-start from-cyan-500 to-blue-500 shadow-lg flex items-center space-x-4 rounded-full">
+        <div className="shrink-0"></div>
+        <div>
+          <div className="text-xl font-medium text-white flex justify-start align-middle h-fit">ChitChat</div>
+          <p className="text-white">You have a new message!</p>
+        </div>
+      </div>
     </div>
   );
 }
